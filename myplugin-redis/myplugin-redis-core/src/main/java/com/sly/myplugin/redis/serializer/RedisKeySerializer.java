d82a7d11a054aca19ceb261b42f89fd50bec4504
sly <1311220942@qq.com>
2021-12-30 16:41:37 +0800

    提交

--- myplugin-redis/myplugin-redis-core/src/main/java/com/sly/myplugin/redis/serializer/RedisKeySerializer.java
+++ myplugin-redis/myplugin-redis-core/src/main/java/com/sly/myplugin/redis/serializer/RedisKeySerializer.java
@@ -4,35 +4,34 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
 import java.io.Serializable;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
+ * redis key serializer
  * @author SLY
  * @date 2021/12/30
  */
 public class RedisKeySerializer implements RedisSerializer<Serializable> {
 
-    private final static Charset CHARSET_UFT8 = Charset.forName("UTF8");
-
     @Override
     public byte[] serialize(Serializable serializable) throws SerializationException {
         if (serializable instanceof String) {
             String s = (String) serializable;
-            return s.getBytes(CHARSET_UFT8);
+            return s.getBytes(StandardCharsets.UTF_8);
         } else if (serializable instanceof Integer) {
-            String s = String.valueOf((Integer) serializable);
-            return (s == null ? null : s.getBytes(CHARSET_UFT8));
+            String s = String.valueOf(serializable);
+            return (s == null ? null : s.getBytes(StandardCharsets.UTF_8));
         } else if (serializable instanceof Long) {
-            String s = String.valueOf((Long) serializable);
-            return (s == null ? null : s.getBytes(CHARSET_UFT8));
+            String s = String.valueOf(serializable);
+            return (s == null ? null : s.getBytes(StandardCharsets.UTF_8));
         } else {
             String s = String.valueOf(serializable);
-            return (s == null ? null : s.getBytes(CHARSET_UFT8));
+            return (s == null ? null : s.getBytes(StandardCharsets.UTF_8));
         }
     }
 
     @Override
     public Serializable deserialize(byte[] bytes) throws SerializationException {
-        return (bytes == null ? null : new String(bytes, CHARSET_UFT8));
+        return (bytes == null ? null : new String(bytes, StandardCharsets.UTF_8));
     }
 }
